987605e447be9c023482b57d4394bb489dac63a1
YametDev <heliyamet3190@gmail.com>
2025-04-03 09:02:51 -0400

    added: Date function

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -1,24 +1,54 @@
-const { application } = require("express");
 const { Application } = require("../models");
+const { DateTime } = require("luxon");
 
 module.exports.lookup = (req, res) => {
-  const find = {
+
+  const clientStartDate = DateTime.fromFormat(req.body.date, "yyyy-MM-dd").startOf("day");
+  const clientEndDate = DateTime.fromFormat(req.body.date, "yyyy-MM-dd").endOf("day");
+
+  // Adjust client date range to UTC using the provided timezone offset
+  const startOfDayUTC = clientStartDate.minus({ minutes: req.body.offset }).toJSDate();
+  const endOfDayUTC = clientEndDate.minus({ minutes: req.body.offset }).toJSDate();
+
+  const find = req.body.date.length === 0 || req.body.date === '0000-00-00' ? {
     company: req.body?.company,
     link: req.body?.link,
     role: req.body?.role,
     state: req.body?.state,
     description: req.body?.description,
+  } : {
+    company: req.body?.company,
+    link: req.body?.link,
+    role: req.body?.role,
+    state: req.body?.state,
+    description: req.body?.description,
+    createdAt: { $gte: startOfDayUTC, $lte: endOfDayUTC }
   };
+  console.log(find);
   Application.countDocuments(find)
     .then((count) => {
       console.log(count);
       Application.find(find)
         .skip(req.body.from)
         .limit(req.body.count)
-        .sort({ votes: 1, views: 1, createdAt: -1 })
+        .sort({ createdAt: -1 })
         .then((applications) => {
           if (applications) {
-            res.send({ result: true, data: applications, count });
+            if (applications.length !== 0) {
+              console.log(applications[0].createdAt);
+            }
+            res.send({
+              result: true,
+              data: applications.map(record => {
+                const utcDate = new Date(record.createdAt);
+                const localDate = new Date(utcDate.getTime() - req.body.offset * 60000);
+                return {
+                  ...record.toObject(),
+                  createdAt: localDate.toISOString().split("T")[0]
+                }
+              }),
+              count
+            });
           } else {
             res.send({ result: false, message: "Applications not found." });
           }
